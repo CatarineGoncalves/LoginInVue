534d72bbccc290d31592487b1d3b5c2cc33c4b87
Catarine <ccatarinegoncalves@gmail.com>
2024-09-05 15:14:20 -0300

    Normalize line endings in .gitignore

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="loginContainer">
+        <div class="inputBox">
+          <Input id="email" v-model="email" inputType="email" label="E-mail" />
+        </div>
+        <div class="inputBox">
+          <Input id="password" v-model="password" inputType="password" label="Senha" />
+        </div>
+        <div class="buttonBox">
+          <Button @click="handleLogin" />
+        </div>
+      </div>
+    </main>
+
+    <Waves />
+  </div>
+</template>
+
+<script>
+import Input from './components/Input/Input.vue';
+import Waves from './components/Waves/Waves.vue';
+import Button from './components/Button/Button.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Input,
+    Waves,
+    Button
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    handleLogin() {
+      if (this.email === '' || this.password === '') {
+        alert('Não deve haver um campo nulo');
+      } else {
+        alert('Está Logado');
+      }
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #8EC5FC;
+  background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+main {
+  width: 300px;
+  height: 29vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+}
+
+.loginContainer div:nth-child(1) {
+  margin-top: 10px;
+}
+
+
+.loginContainer {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  place-self: center;
+}
+
+.inputBox {
+  margin-bottom: 10px;
+}
+
+
+@media only screen and (max-width: 445px) {
+  main {
+    width: 90vw;
+  }
+}
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button 
+    class="loginButton" 
+    :class="{ 'clicked': isClicked }"
+    @click="handleClick"
+  >
+    <img src="./../../assets/entrar.png" alt="Login" />
+    Login
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  data() {
+    return {
+      isClicked: false
+    };
+  },
+  methods: {
+    handleClick() {
+      this.isClicked = true;
+      setTimeout(() => {
+        this.isClicked = false;
+      }, 300);
+      this.$emit('click'); 
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @import './Button.css';
+</style>
